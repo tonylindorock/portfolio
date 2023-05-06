79dd3ecfe918ec4b6e32f5824b4f6ee0ba490eb4
tonylindorock <tonylindorock@gmail.com>
2023-05-06 17:38:55 -0400

    Updated project listing pages

--- js/script_inventory.js
+++ js/script_inventory.js
@@ -1,1 +1,17 @@
 "use strict"
+
+
+function onProjBtnEntered(e){
+    var vid = e.children[0];
+    if (vid.tagName === "VIDEO"){
+        vid.play();
+    }
+}
+
+function onProjBtnExited(e){
+    var vid = e.children[0];
+    if (vid.tagName === "VIDEO"){
+        vid.pause();
+        vid.currentTime = 0;
+    }
+}
